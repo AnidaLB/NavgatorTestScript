a1030f8b4f562a310870165f39e0b5c18d54654f
AnidaLB <anidabeg@gmail.com>
2018-11-20 13:45:13 +0100

    Smoke3
    Matcher added

--- assert_spec.rb
+++ assert_spec.rb
@@ -14,21 +14,29 @@ end
 
 
 
-describe 'Navigator test' do
+describe 'Navigator smoke test' do
 	context "When searching for a place" do
 
 
 it 'should open the main page' do
- @browser.goto('http://www.navigator.ba/#/categories')
+ @browser.goto('http://www.navigator.ba/')
 end 
 
+it 'should check if properly redirected' do
+	expect(@browser.url).to eq('http://www.navigator.ba/#/categories')
+	puts @browser.url
+	sleep 2
+end
+
 it 'should enter the name of the place' do
-	@browser.text_field(:xpath => '//*[@id="ember558"]').send_keys "atlantbh"
+	@browser.input(:xpath => '//*[@id="ember564"]').send_keys "atlantbh"
 end
 
 it 'should click on search icon' do
-	@browser.span(:xpath => '//*[@id="ember549"]/a/span').click
+	@browser.span(:xpath => '//*[@id="ember555"]/a/span').click
 end
+
+# 
 
 it 'should open the place from search results' do
 	@browser.element(title: "Atlantbh").click
@@ -40,6 +48,7 @@ it 'should check if the place is shown on the map' do
 
 	@atlant = @browser.element(class: "leaflet-popup-content")
 	expect(@atlant).to exist
+
 
 end
 
