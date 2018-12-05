637a385155a174e6ecbe30a073ada3441ccf5189
AnidaLB <anidabeg@gmail.com>
2018-12-05 22:21:06 +0100

    Open Atlantbh site from Navigator

---
+++ atlants_page_spec.rb
@@ -0,0 +1,37 @@
+require 'rspec'
+require 'watir'
+require 'selenium-webdriver'
+
+browser = Watir::Browser.new :chrome
+
+RSpec.configure do |config|
+  config.color = true
+  config.formatter = :documentation
+  config.before(:each) { @browser = browser }
+  config.before(:all) {browser.window.maximize}
+  config.after(:suite) { browser.close unless browser.nil? }
+end
+
+describe 'Navigator regression test' do
+	context 'When opening Atlantbh web page' do
+
+		it 'should open Navigator' do
+			@browser.goto('http://www.navigator.ba/')
+		end
+
+		it 'should check if properly redirected' do
+			expect(@browser.url).to eq('http://www.navigator.ba/#/categories')
+			puts "Redirected to:" + @browser.url
+		end
+
+		it 'should open the page of Atlantbh' do
+			@browser.a(:xpath => '//*[@id="ember628"]/div[3]/div[4]/div/a[2]').click
+		end
+
+		it 'should check if the page is opened' do
+			@browser.window(:url => /atlantbh/).use
+			@browser.url
+			expect(@browser.url).to eq('https://www.atlantbh.com/')
+		end
+	end
+end
